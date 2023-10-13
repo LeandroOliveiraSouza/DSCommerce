97010f6c72bc895a15f0a7bc5c92ae3448f30007
LeandroOliveiraSouza <leandro_oliveirasouza@outlook.com>
2023-10-13 16:42:34 -0300

    Paginated product search

--- backend/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ backend/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -3,6 +3,8 @@ package com.devsuperior.dscommerce.controllers;
 import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +18,12 @@ public class ProductController {
     private ProductService service;
 
     @GetMapping(value = "/{id}")
-    public ProductDTO findById(@PathVariable Long id) {
+    public ProductDTO findById(@PathVariable Long id) throws Exception {
        return service.findById(id);
+    }
+
+    @GetMapping
+    public Page<ProductDTO> findAll(Pageable pageable) throws Exception {
+        return service.findAll(pageable);
     }
 }
--- backend/src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -5,6 +5,8 @@ import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -16,8 +18,20 @@ public class ProductService {
     private ProductRepository repository;
 
     @Transactional
-    public ProductDTO findById(Long id) {
-        Product product = repository.findById(id).get();
-        return new ProductDTO(product);
+    public ProductDTO findById(Long id) throws Exception {
+        Optional<Product> optionalProduct = repository.findById(id);
+        if (optionalProduct.isPresent()) {
+            Product product = optionalProduct.get();
+            return new ProductDTO(product);
+        }
+        else {
+            throw new Exception("Product not found for id: " + id);
+        }
+    }
+
+    @Transactional
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> product = repository.findAll(pageable);
+        return product.map(ProductDTO::new);
     }
 }
