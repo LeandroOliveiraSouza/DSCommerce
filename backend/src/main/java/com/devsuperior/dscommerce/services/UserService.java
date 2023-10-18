869974e8a0a221f75de14d28986aeb67cad43876
LeandroOliveiraSouza <leandro_oliveirasouza@outlook.com>
2023-10-18 18:13:27 -0300

    Getting the logged in user

--- backend/src/main/java/com/devsuperior/dscommerce/services/UserService.java
+++ backend/src/main/java/com/devsuperior/dscommerce/services/UserService.java
@@ -1,16 +1,22 @@
 package com.devsuperior.dscommerce.services;
 
+import com.devsuperior.dscommerce.dto.UserDTO;
 import com.devsuperior.dscommerce.entities.Role;
 import com.devsuperior.dscommerce.entities.User;
 import com.devsuperior.dscommerce.projection.UserDetailsProjection;
 import com.devsuperior.dscommerce.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -31,7 +37,31 @@ public class UserService implements UserDetailsService {
         for (UserDetailsProjection projection : result) {
             user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
         }
-
         return user;
+    }
+
+    protected User authenticated() {
+        try {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
+            String username = jwtPrincipal.getClaim("username");
+
+            Optional<User> optionalUser = repository.findByEmail(username);
+            if (optionalUser.isEmpty()) {
+                throw new UsernameNotFoundException("Email not found");
+            }
+            else {
+                return optionalUser.get();
+            }
+        }
+        catch (Exception e) {
+            throw new UsernameNotFoundException("Email not found");
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public UserDTO getMyself() {
+        User user = authenticated();
+        return new UserDTO(user);
     }
 }
